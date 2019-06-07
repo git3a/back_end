21b5d88d0b7f991c0490e11601508fc65deaa1ae
SUN HONG <523882907@qq.com>
2019-06-08 01:43:37 +0900

    add wsgi

--- back_end/wsgi.py
+++ back_end/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+sys.path.append('/opt/bitnami/apps/django/django_projects/back_end')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/back_end/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "back_end.settings")
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back_end.settings')
-
 application = get_wsgi_application()
