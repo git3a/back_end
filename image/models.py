4102e94248e6ca20c34af957717caa882590b7a3
sun hong <git3agroup@gami.com>
2019-06-23 11:03:57 +0900

    image server

---
+++ image/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from datetime import datetime
+from django.conf import settings
+
+class UploadImage(models.Model):
+    class Meta:
+        db_table = "upload_image"
+
+    id = models.IntegerField(primary_key=True)
+    filename = models.CharField(max_length=252, default="")
+    file_md5 = models.CharField(max_length=128)
+    file_type = models.CharField(max_length=32)
+    file_size = models.IntegerField()
+    created_at = models.DateTimeField(default=datetime.now)
+    updated_at = models.DateTimeField(default=datetime.now)
+  
+    # 我们还定义了通过文件md5值获取模型对象的类方法
+    @classmethod
+    def getImageByMd5(cls, md5):
+        try:
+            return UploadImage.objects.filter(file_md5=md5).first()
+        except Exception as e:
+            return None
+
+    # 获取本图片的url，我们可以通过这个url在浏览器访问到这个图片
+    # 其中settings.WEB_HOST_NAME 是常量配置，指你的服务器的域名
+    # settings.WEB_IMAGE_SERVER_PATH 也是常量配置，指你的静态图片资源访问路径
+    # 这些配置项我在Django项目的settings.py文件中进行配置
+    def getImageUrl(self):
+        filename = self.file_md5 + "." + self.file_type
+        url = settings.WEB_HOST_NAME + settings.WEB_IMAGE_SERVER_PATH + filename
+        return url
+
+    # 获取本图片在本地的位置，即你的文件系统的路径，图片会保存在这个路径下
+    def getImagePath(self):
+        filename = self.file_md5 + "." + self.file_type
+        path = settings.STATIC_URL + filename
+        return path
+
+    def __str__(self):
+        s = "filename:" + str(self.filename) + " - " + "filetype:" + str(self.file_type) \
+        + " - " +  "filesize:" + str(self.file_size) + " - " + "filemd5:" + str(self.file_md5)
+        return s
+
+# Create your models here.
---
+++ image/uploadImage.py
@@ -0,0 +1,72 @@
+from django.views.decorators.http import require_http_methods
+import filetype, hashlib
+from image.views import returnOk, returnForbidden, returnBadRequest
+from image.models import UploadImage
+from django.conf import settings
+
+# 上传文件的视图
+@require_http_methods(["POST"])
+def uploadImage(request):
+    # 从请求表单中获取文件对象
+    file = request.FILES.get("img", None)
+    if not file:  # 文件对象不存在， 返回400请求错误
+        return returnBadRequest("need file.")
+
+    # 计算文件md5
+    md5 = pCalculateMd5(file)
+    uploadImg = UploadImage.getImageByMd5(md5)
+    if uploadImg:   # 图片文件已存在， 直接返回
+        return returnOk({'url': uploadImg.getImageUrl()})
+
+    # 获取扩展类型 并 判断
+    ext = pGetFileExtension(file)
+    if not pIsAllowedImageType(ext):
+        return returnForbidden("文件类型错误")
+
+    # 检测通过 创建新的image对象
+    # 文件对象即上一小节的UploadImage模型
+    uploadImg = UploadImage()
+    uploadImg.filename = file.name
+    uploadImg.file_size = file.size
+    uploadImg.file_md5 = md5
+    uploadImg.file_type = ext
+    uploadImg.save()  # 插入数据库
+
+    # 保存 文件到磁盘
+    with open(uploadImg.getImagePath(), "wb+") as f:
+        # 分块写入
+        for chunk in file.chunks():
+            f.write(chunk)
+
+
+    # 返回图片的url以供访问
+    return returnOk({"url": uploadImg.getImageUrl()})
+
+
+# 检测文件类型
+# 我们使用第三方的库filetype进行检测，而不是通过文件名进行判断
+# pip install filetype 即可安装该库
+def pGetFileExtension(file):
+    rawData = bytearray()
+    for c in file.chunks():
+        rawData += c
+    try:
+        ext = filetype.guess_extension(rawData)
+        return ext
+    except Exception as e:
+        # todo log
+        return None
+
+# 计算文件的md5
+def pCalculateMd5(file):
+    md5Obj = hashlib.md5()
+    for chunk in file.chunks():
+        md5Obj.update(chunk)
+    return md5Obj.hexdigest()
+
+# 文件类型过滤 我们只允许上传常用的图片文件
+def pIsAllowedImageType(ext):
+    if ext in ["png", "jpeg", "jpg"]:
+        return True
+    return False
+
