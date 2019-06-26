bacdcef5005e60102e8112c8ff57830decf8874f
sun <sun@gmail.com>
2019-06-26 14:58:21 +0900

    add

--- back_end/wsgi.py
+++ back_end/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for back_end project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
 import os
+from os.path import join,dirname,abspath
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+
 import sys
-sys.path.append('/opt/bitnami/apps/django/django_projects/back_end')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/back_end/egg_cache")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "back_end.settings")
+sys.path.insert(0,PROJECT_DIR)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "examsys.settings")
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
