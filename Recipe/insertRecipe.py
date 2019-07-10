ecd6490b5a828e41bfd3c254d18cdec948519981
WANG Zhibin <234@32.com>
2019-07-10 17:59:54 +0900

    UserId can be saved as a foreign key

--- Recipe/insertRecipe.py
+++ Recipe/insertRecipe.py
@@ -1,16 +1,23 @@
 from django.http import HttpResponse
 from Recipe.models import Recipe
+import pymysql
+from User.models import UserInfo
 
 def insertRecipe(request):
+
+    user_id = request.GET.get("user_id")
+    user = UserInfo.objects.get(id=user_id) # Get user_id instance
     recipe_name = request.GET.get("name")
     recipe_image = request.GET.get("img_url")
     recipe_material = request.GET.get("material")
     recipe_amount = request.GET.get("amount")
     recipe_steps = request.GET.get("steps")
     recipe_time  = request.GET.get("time")
-    data = Recipe(name=recipe_name, image=recipe_image, material=recipe_material, amount=recipe_amount,
+    data = Recipe(userid=user,name=recipe_name, image=recipe_image, material=recipe_material, amount=recipe_amount,
                   step=recipe_steps, time=recipe_time)
+
     data.save()
     return HttpResponse()
+
 
 
--- Recipe/models.py
+++ Recipe/models.py
@@ -1,30 +1,31 @@
-from django.db import models
 import json
 
+from django.db import models
 
 
 class UserEncoder(json.JSONEncoder):
     def default(self, obj):
-        if isinstance(obj,UserInfo):
+        if isinstance(obj, UserInfo):
             return obj.name
-        return json.JSONEncoder.default(self,obj)
-# Create your models here.
-class Recipe(models.Model):
-	# Recipeid: INTEGRAL型で，主キー
-	
-	#recipeid = models.IntegerField(primary_key=True)
-	# 名前：文字列100桁
-	name = models.TextField()
-	# 図
-	image = models.TextField()
-	# material
-	material = models.TextField()
-	# 量
-	amount = models.TextField()
-	# ステップ
-	step = models.TextField()
-	time = models.TextField()
+        return json.JSONEncoder.default(self, obj)
 
-# class UserRecipe(models.Model):
-# 	userid
-# 	recipeid
+
+# Create your models here.
+
+class Recipe(models.Model):
+    # Recipeid: INTEGRAL型で，主キー
+    # recipeid = models.IntegerField(primary_key=True)
+    userid = models.ForeignKey('User.UserInfo',on_delete=models.CASCADE)
+
+    # 名前：文字列100桁
+    name = models.TextField()
+    # 図
+    image = models.TextField()
+    # material
+    material = models.TextField()
+    # 量
+    amount = models.TextField()
+    # ステップ
+    step = models.TextField()
+    # 時間
+    time = models.TextField()
