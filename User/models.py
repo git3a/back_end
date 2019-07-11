3367a4ee06399798b11e5e9a7a93761f06bb7cc0
sun hong <git3agroup@gami.com>
2019-07-11 11:51:09 +0900

    user

--- User/models.py
+++ User/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class UserInfo(models.Model):
 
 	#userid = models.CharField(max_length=100)
-	user = models.CharField(max_length=100,unique=True)
-	pwd = models.CharField(max_length=100)
+	user = models.CharField(max_length=64,unique=True)
+	pwd = models.CharField(max_length=64)
 	email = models.EmailField(unique=True,default="@")
-	touxiang = models.CharField(max_length = 100,default="")
+	touxiang = models.TextField("")
 	#sex = models.CharField(max_length=32, choices=gender, default="男")
 
 class UserFavorite(models.Model):
